5327565b6ee587dcdc0b2bcc502b9eb8b09806eb
jonathanarmando434 <jonathanarmando434@gmail.com>
2025-03-29 10:56:13 +0100

    update README.md to reflect completed functional requirements and add account statement retrieval endpoint in index.js

--- src/index.js
+++ src/index.js
@@ -9,13 +9,27 @@ const accounts = [];
 
 app.post('/account', (req, res) => {
     const { cpf, name} = req.body;
+    const accountAlreadyExists = accounts.some(
+        account => account.cpf === cpf
+    );
+    if(accountAlreadyExists) return res.status(400).json({ error: "CPF already exists" })
     accounts.push({
         id: uuidv4(),
         cpf,
         name,
         statement: []
     });
-    res.status(201).send();
+    return res.status(201).send();
 });
+
+app.get('/account/statement/:cpf', (req, res) => {
+    const {cpf} = req.params;
+
+    const account = accounts.find(account => account.cpf === cpf);
+
+    if(!account) return res.status(400).json({ error: "Account not found" });
+
+    return res.json(account.statement);
+})
 
 app.listen(3333);
