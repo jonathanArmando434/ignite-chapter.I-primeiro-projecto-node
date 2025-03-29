c1988fcf242fda48b612d0f5dda326e50f4c5562
jonathanarmando434 <jonathanarmando434@gmail.com>
2025-03-29 14:13:13 +0100

    refactor account statement handling and add withdraw functionality

--- src/index.js
+++ src/index.js
@@ -19,24 +19,37 @@ const verifyAccountExistsByCPF = (req, res, next) => {
     next();
 }
 
+const getBalance = (statements) => {
+    const balance = statements.reduce((acc, statement) =>{
+        if(statement.type === 'credit') return acc + statement.amount
+        return acc - statement.amount 
+    }, 0);
+
+    return balance;
+}
+
 app.post('/account', (req, res) => {
     const { cpf, name} = req.body;
+
     const accountAlreadyExists = accounts.some(
         account => account.cpf === cpf
     );
+
     if(accountAlreadyExists) return res.status(400).json({ error: "CPF already exists" })
+
     accounts.push({
         id: uuidv4(),
         cpf,
         name,
-        statement: []
+        statements: []
     });
+
     return res.status(201).send();
 });
 
 app.get('/account/statement', verifyAccountExistsByCPF, (req, res) => {
     const { account } = req;
-    return res.json(account.statement);
+    return res.json(account.statements);
 })
 
 app.post('/account/deposit', verifyAccountExistsByCPF, (req, res) =>{
@@ -51,9 +64,28 @@ app.post('/account/deposit', verifyAccountExistsByCPF, (req, res) =>{
         description
     }
 
-    account.statement.push(statement);
+    account.statements.push(statement);
 
-    return res.json(account.statement);
+    return res.json(account.statements);
 })
+
+app.post('/account/withdraw', verifyAccountExistsByCPF, (req, res) => {
+    const { amount } = req.body;
+    const { account } = req;
+
+    const balance = getBalance(account.statements);
+
+    if(balance < amount) return res.status(400).json({ error: 'You can not withdraw this amount' });
+
+    const statement = {
+        amount,
+        createdAt: new Date(),
+        type: 'debit'
+    }
+
+    account.statements.push(statement);
+
+    return res.json(account.statements);
+}) 
 
 app.listen(3333);
